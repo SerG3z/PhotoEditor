c1ab75ffd1aa1a678e81fadd6b7de264bd2364dc
serg3z <serg3z.hard@gmail.com>
2016-05-27 15:29:12 +0600

    add new fragments for selected preview photo and selected completed photo

--- app/src/main/java/com/example/serg3z/photoeditor/fragment/DialogItemResult.java
+++ app/src/main/java/com/example/serg3z/photoeditor/fragment/DialogItemResult.java
@@ -1,7 +1,54 @@
 package com.example.serg3z.photoeditor.fragment;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.example.serg3z.photoeditor.R;
+
 /**
  * Created by serg3z on 15.05.16.
  */
-public class DialogItemResult {
+public class DialogItemResult extends DialogFragment {
+
+    private static final String POSITION_KEY = "position";
+    private DialogItemResultListener listener;
+    private int position;
+
+    public static DialogItemResult newIntent(int position) {
+        DialogItemResult dialogItemResult = new DialogItemResult();
+        Bundle arguments = new Bundle();
+        arguments.putInt(POSITION_KEY, position);
+        dialogItemResult.setArguments(arguments);
+        return dialogItemResult;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        position = getArguments().getInt(POSITION_KEY);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        this.listener = (DialogItemResultListener) getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.dialog_title_item_result)
+                .setItems(R.array.array_dialog_item_result, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (listener != null) {
+                            listener.onDialogItemResultClicked(which, position);
+                        }
+                        dialog.dismiss();
+                    }
+                });
+        return builder.create();
+    }
+
+    public interface DialogItemResultListener {
+        void onDialogItemResultClicked(int id, int position);
+    }
+
 }
--- app/src/main/java/com/example/serg3z/photoeditor/fragment/DialogSelectPhoto.java
+++ app/src/main/java/com/example/serg3z/photoeditor/fragment/DialogSelectPhoto.java
@@ -1,12 +1,10 @@
 package com.example.serg3z.photoeditor.fragment;
 
 import android.app.AlertDialog;
-import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.example.serg3z.photoeditor.R;
 
@@ -15,13 +13,13 @@ import com.example.serg3z.photoeditor.R;
  */
 public class DialogSelectPhoto extends DialogFragment {
 
-    DialogFragmentListener listener;
+    private DialogFragmentListener listener;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         this.listener = (DialogFragmentListener) getActivity();
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.dialog_about_title)
+        builder.setTitle(R.string.dialog_title_take_photo)
                 .setItems(R.array.array_dialog_take_photo, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         if (listener != null) {
